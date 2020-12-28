eb733ff4a95e55dcd1ff217009c395f647b4136b
olaf-snow <olaf.snow@navercorp.com>
2020-12-28 17:21:16 +0900

    :sparkles: setup router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {Router, Link} from '@reach/router'
+
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+function About() {
+  return (
+    <div>About</div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Link to="/">HOME</Link>
+        <Link to="/about">ABOUT</Link>
+      </div>
+      <Router>
+        <Home path="/" />
+        <About path="/about" />
+      </Router>
     </div>
   );
 }
