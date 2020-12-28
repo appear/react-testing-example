7a509a576cd59588e7a99e69071c1a1c60018666
olaf-snow <olaf.snow@navercorp.com>
2020-12-28 18:16:47 +0900

    ::white_check_mark: router test 코드 추가

---
+++ src/__test__/router.test.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import {
+  createHistory,
+  createMemorySource,
+  LocationProvider,
+} from '@reach/router'
+import '@testing-library/jest-dom/extend-expect'
+
+import App from '../App'
+
+function renderWithRouter(ui, router) {
+  const { route = '/', history = createHistory(createMemorySource(route)) } =
+    router || {}
+
+  return {
+    ...render(<LocationProvider history={history}>{ui}</LocationProvider>),
+    history,
+  }
+}
+
+describe('Router Test', () => {
+  it('첫 진입시 기본 페이지로 이동한다', () => {
+    const { container } = renderWithRouter(<App />)
+
+    expect(container.innerHTML).toMatch('Home')
+  })
+  it('/about path 로 진입시 About Component 를 노출한다', () => {
+    const { container } = renderWithRouter(<App />, {
+      route: '/about',
+    })
+
+    expect(container.innerHTML).toMatch('About')
+  })
+  it('/about navigating', async () => {
+    const { container, history } = renderWithRouter(<App />)
+
+    await history.navigate('/about')
+
+    expect(container.innerHTML).toMatch('About')
+  })
+})
